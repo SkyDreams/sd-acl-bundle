0fdeba2c431b25639d8febd3175cb44487db6590
Mateusz Jozwik <mateusz.jozwik@skydreams.com>
2026-04-20 16:10:44 +0200

    PLAT-3491 - allow symfony 8.0

--- src/DependencyInjection/AclExtension.php
+++ src/DependencyInjection/AclExtension.php
@@ -30,7 +30,7 @@ class AclExtension extends Extension
     /**
      * {@inheritdoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $mainConfig = $this->getConfiguration($configs, $container);
         $config = $this->processConfiguration($mainConfig, $configs);
